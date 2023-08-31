53b1caaf919496bbea62a9fef57f751459d54d57
abdulhamid4663 <abdulhamid4663@gmail.com>
2023-09-01 04:44:05 +0600

    finished blog's second question

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ const loadCategories = async (isSorted) => { // Load Categories Data
 
 const showCategories = (categories, isSorted) => { // Show Categories Data
     const categoriesContainer = document.getElementById('categories-container');
-    categoriesContainer.textContent= '';
-    
+    categoriesContainer.textContent = '';
+
     categories.forEach(category => { // Each Category Data
         const newDiv = document.createElement('div');
         newDiv.innerHTML = `
-            <button onclick="loadVideosData('${category.category_id}')" class="btn text-lg font-medium text-[#252525B2] normal-case">${category.category}</button>
-        `;
+            <button onclick="loadVideosData('${category.category_id}', '${isSorted}')" class="btn text-lg font-medium text-[#252525B2] normal-case">${category.category}</button>
+            `;
         categoriesContainer.appendChild(newDiv);
     });
 };
@@ -22,43 +22,43 @@ const showCategories = (categories, isSorted) => { // Show Categories Data
 const loadVideosData = async (videosId, isSorted) => { // Load Videos Data
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${videosId}`);
     const data = await res.json();
-    const videos = data.data;
+    let videos = data.data;
 
+    if (isSorted === "true") {
+        videos = videos.sort((a, b) => parseFloat(b.others.views) - parseFloat(a.others.views));
+    };
+    
     showVideos(videos);
 };
 
 const showVideos = (videos) => { // Show Videos Data
     const videosContainer = document.getElementById('videos-container');
-    const errorDiv = document.getElementById('errorDiv');
+    const errorDiv = document.getElementById('errorDiv'); 
 
-    if(videos.length === 0) {
+    if (videos.length === 0) {
         errorDiv.classList.add('flex');
         errorDiv.classList.remove('hidden');
     } else {
         errorDiv.classList.add('hidden');
     };
-    
-    videosContainer.textContent = "";
 
+    videosContainer.textContent = "";
     videos.forEach(video => { // Each Videos Data
         const newDiv = document.createElement('div');
-
-        const postTime = Number.parseFloat(video.others.posted_date);
-        const toSeconds = Number.parseInt(postTime / 1000);
-        const toMinutes = Number.parseInt(toSeconds / 60);
-        const toHours = Number.parseInt(toSeconds / 3600);
-        const toDays = Number.parseInt(toHours / 24);
-
-        console.log(postTime)
+        
+        const seconds = Number.parseInt(video?.others?.posted_date);
+        const toMinutes = seconds / 60;
+        const minutesFormat = Number.parseInt(toMinutes % 60);
+        const hoursFormat = Number.parseInt(toMinutes / 60);
 
         newDiv.innerHTML = `
             <div class="card-compact">
-                <figure>
+                <figure class="relative">
                     <img src="${video?.thumbnail ? video.thumbnail : "No thumbnail available."}" alt="${video?.title}" class=" w-full h-[14.6875rem] rounded-xl cursor-pointer"/>
-                    
+                    ${video.others.posted_date ? `<h1 class="bg-[#171717] text-white text-[0.625rem] font-normal absolute bottom-2 right-3 p-1 rounded-md cursor-pointer">${hoursFormat}h ${minutesFormat}m ago</h1>` : ""}
                 </figure>
                 <div class="flex pt-5 gap-3 ">
-                    <img src="${video?.authors[0].profile_picture}" alt="${video?.title}" class=" w-10 h-10 rounded-full cursor-pointer" />
+                    <img src="${video?.authors[0]?.profile_picture}" alt="${video?.title}" class=" w-10 h-10 rounded-full cursor-pointer" />
                     <div>
                         <h2 class="card-title text-base font-bold text-[#171717] cursor-pointer">${video?.title ? video.title : ""}</h2>
                         <div class="flex gap-2 items-center">
@@ -71,7 +71,6 @@ const showVideos = (videos) => { // Show Videos Data
             </div>
         `;
         videosContainer.appendChild(newDiv);
-        console.log(video);
     });
 };
 
@@ -79,4 +78,9 @@ const sortByView = () => {
     loadCategories(true);
 };
 
+const showBlogModal = () => {
+    my_modal_1.showModal()
+};
+
 loadCategories();
+loadVideosData("1000");
